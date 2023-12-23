be8f6d62b9bafb104c3fd3d8fdcf28bfea63aad2
Nishanth <nishukiran43@gmail.com>
2023-12-23 10:56:58 +0530

    yt scrollview added

---
+++ src/navigation/Routes.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/Home';
+import YtScrollView from '../screens/YtScrollView';
+
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                {/* <Stack.Screen name={'Home'} component={Home} /> */}
+                <Stack.Screen name={'YtScrollView'} component={YtScrollView} />
+            </Stack.Navigator>
+
+        </NavigationContainer>
+    );
+}
